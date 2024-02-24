2dab6499bc7af88b56caead9b096187c1e8f0b41
yuuthod <yuuthod@gmail.com>
2024-02-24 16:29:18 +0900

    ✨ feat: Add ThemeEditButton Component
    - ThemeEditButton 컴포넌트의 기본 구조를 구현
    - 해당 버튼은 테마 선택 후 사이트 수정을 위한 에디터 기능을 활성화하는 역할
    - 현재 다운로드, 테마 변경, 컬러 변경 등의 기능은 placeholder 상태로 존재
    - Note: 현재 기능들은 WIP(Work In Progress) 상태이며, 추후 업데이트를 통해 완성될 예정

---
+++ components/ThemeEditButton/index.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import Image from 'next/image';
+import adIcon from '@/public/icons/ad.svg';
+import doneOutlineIcon from '@/public/icons/done_outline.svg';
+import downloadIcon from '@/public/icons/download.svg';
+import stylusNoteIcons from '@/public/icons/stylus_note.svg';
+import palette from '@/public/icons/palette.svg';
+import cn from 'classnames';
+import { useState } from 'react';
+import style from './style.module.css';
+
+export default function ThemeEditButton() {
+  const [isOpen, setIsOpen] = useState(true);
+  function handleClickDropdown(open: boolean) {
+    return () => setIsOpen(open);
+  }
+  return (
+    <div
+      role='button'
+      className={cn(style.dropdown, isOpen ? style.open : style.close)}
+    >
+      <button
+        type='button'
+        className={style.edit}
+        onClick={handleClickDropdown(true)}
+      >
+        <Image priority src={stylusNoteIcons} alt='수정' />
+      </button>
+      <ul className={style['button-list']}>
+        <li>
+          <button type='button'>
+            <Image priority src={downloadIcon} alt='다운로드' />
+          </button>
+        </li>
+        <li>
+          <button type='button'>
+            <Image priority src={adIcon} alt='테마변경' />
+          </button>
+        </li>
+        <li>
+          <button type='button'>
+            <Image priority src={palette} alt='컬러수정' />
+          </button>
+        </li>
+      </ul>
+      <button
+        type='button'
+        className={style.completed}
+        onClick={handleClickDropdown(false)}
+      >
+        <Image priority src={doneOutlineIcon} alt='수정완료' />
+      </button>
+    </div>
+  );
+}
